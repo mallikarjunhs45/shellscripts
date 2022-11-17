75743b49d1ac82bfa45bc016f2370e8ccfaab416
“sharath” <kjsharath5@gmail.com>
2022-11-17 10:09:46 +0000

    1st commit for b1

--- s3.sh
+++ s3.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ls > file13
-echo "All the files in the directory are listed below"
-cat file13 cat file4 testing for vinish
+echo "All the files in the directory are listed below"D
+cat file13
