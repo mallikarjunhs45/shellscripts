068fe263f3f51572b553d2090d7a6e8e59ccd008
“sharath” <kjsharath5@gmail.com>
2022-11-19 11:24:41 +0000

    commiting the file

--- clone.sh
+++ clone.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+git clone https://github.com/sharathkj03/myrepo.git
 echo "enter the file name to create"
 read string
 vi $string
 git add .
-git commit -m "commiting the file"
+git commit -m  "commiting the file"
+git push origin -- global $string
 
