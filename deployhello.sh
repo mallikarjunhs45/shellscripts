2f7e37a583814e7f920c948810816055f4c2a09e
sharathkj03 <118235858+sharathkj03@users.noreply.github.com>
2022-11-28 12:43:03 +0530

    Update deployhello.sh

--- deployhello.sh
+++ deployhello.sh
@@ -5,6 +5,6 @@ git clone https://github.com/sharathkj03/hello-world-war.git
 cd hello-world-war
 mvn package
 sudo cp -r /home/devops/workspace/To build and deploy/hello-world-war/targethello-world-war-1.0.0.war /home/devops/workspace/Installing tomcat and maven/conf-and-webapps-file/
-sh /opt/apache-tomcat-10.0.27/bin/shutdown.sh
-sleep 3
-sh /opt/apache-tomcat-10.0.27/bin/startup.sh
+sudo sh /opt/tomcat/bin/shutdown.sh
+sleep 2
+sudo sh /opt/tomcat/bin/startup.sh
